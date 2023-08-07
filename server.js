0813e6f5a2376f413060e6f54e12be9d5695ad99
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-08-07 22:42:34 +0600

    file destination add

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const bodyParser = require("body-parser");
 const multer = require("multer")
 const dotenv = require("dotenv").config();
 const app = express();
+
 app.use(bodyParser.json());
-const db_connect = require("./config/db");
+const upload = multer({dest :"./public/data/uploads"})
+
+const db_connect = require("./config/db"); 
 
 //! MongoDb connection
 db_connect();
@@ -13,6 +16,12 @@ db_connect();
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Task Manager app" });
 });
+
+// Multer(File upload)
+app.post("/uploads",(req,res)=>{
+  res.json({ message: "Upload completed" });
+})
+
 //! Routes
 app.use("/api/visitors", require("./routes/api/route"));
 app.use("/api/tasks", require("./routes/api/tasks"));
