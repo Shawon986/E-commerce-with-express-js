2645eacfb2d9497f5075560251c4e02cf26f2556
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-08-10 00:43:40 +0600

    fileobj modified

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const multer = require("multer")
 const dotenv = require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/uploads/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + require("crypto").randomBytes(64).toString("hex")+ '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + '-'+ file.originalname)
-  }
-}) 
 
-const upload = multer({ storage: storage })
 
 const db_connect = require("./config/db");  
 
@@ -28,10 +17,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Task Manager app" });
 });
 
-// Multer(File upload)
-app.post("/uploads",upload.single("file"),(req,res)=>{
-  res.json(req.file);
-})
 
 //! Routes
 app.use("/api/visitors", require("./routes/api/route"));
